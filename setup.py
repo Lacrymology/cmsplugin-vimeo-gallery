b72f092b7e20823d7e7fbcecde01aa7902482973
Tomas Neme <lacrymology@gmail.com>
2013-03-01 16:22:47 -0300

    add setup and MANIFEST files

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='cmsplugin-vimeo-gallery',
+    version='0.0.0',
+    author='Tomas Neme',
+    author_email='lacrymology@gmail.com',
+    url='http://github.com/Lacrymology',
+    description = 'DjangoCMS vimeo embedded video gallery plugin with '
+                  'drag&drop reordering in admin',
+    packages=find_packages(),
+    provides=['cmsplugin-vimeo-gallery', ],
+    include_package_data=True,
+    install_requires = [
+        ],
+    package_data={
+        'cmsplugin_vimeo_gallery': [
+            'templates/cmsplugin_vimeo_gallery/*.html',
+            'static/js/*.js'
+        ]
+    },
+)
