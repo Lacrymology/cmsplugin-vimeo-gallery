389088465d73e30a5c950c2078c020ec773cd060
Tomas Neme <lacrymology@gmail.com>
2013-03-01 16:28:23 -0300

    cms plugin file

---
+++ cmsplugin_vimeo_gallery/cms_plugins.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: cmsplugin_vimeo_gallery.cms_plugins
+   :platform: Unix
+   :synopsis: TODO
+
+.. moduleauthor:: Tomas Neme <lacrymology@gmail.com>
+
+"""
