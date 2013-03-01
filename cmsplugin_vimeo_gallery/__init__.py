2f1c304ecc8e37855d363836e324dbfd9aec9e46
Tomas Neme <lacrymology@gmail.com>
2013-03-01 16:28:13 -0300

    base package

---
+++ cmsplugin_vimeo_gallery/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: cmsplugin_vimeo_gallery
+   :platform: Unix
+   :synopsis: Base package for the video gallery django-cms plugin
+
+.. moduleauthor:: Tomas Neme <lacrymology@gmail.com>
+
+"""
---
+++ cmsplugin_vimeo_gallery/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: cmsplugin_vimeo_gallery.models
+   :platform: Unix
+   :synopsis: TODO
+
+.. moduleauthor:: Tomas Neme <lacrymology@gmail.com>
+
+"""
